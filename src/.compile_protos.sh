bc245c173a088f3fdab8752e67292f17bce4a41b
Matteo Gianella <gianeteo@hotmail.it>
2020-09-16 11:42:41 +0200

    Code reorganized, make proto file available on top directory of installed package

---
+++ src/.compile_protos.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir cpp_proto
+PROTO_SRC=../inst/proto
+for elem in $PROTO_SRC/*.proto; do protoc -I$PROTO_SRC --cpp_out=$PWD/cpp_proto $elem; done
+
--- src/protos/.compile_protos.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-PROTO_SRC=$PWD/protos
-for elem in $PROTO_SRC/*.proto; do protoc -I$PROTO_SRC --cpp_out=$PROTO_SRC $elem; done
-
