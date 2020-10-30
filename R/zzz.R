8021d98b2584ab545b067d6ba536922eb449bf60
Matteo Gianella <gianeteo@hotmail.it>
2020-10-30 23:09:29 +0100

    Removed .onLoad utility

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,3 @@
-# Load RProtoBuf automatically when this package is loaded
-.onLoad <- function (libname, pkgname) {
-  require(RProtoBuf)
-}
-
 # Unloading the dynamic library when SPMIX is unloaded
 .onUnload <- function (libpath) {
   library.dynam.unload("SPMIX", libpath)
