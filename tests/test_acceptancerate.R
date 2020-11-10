d2a369171dba4d79c3861f9d5939944432c25630
Matteo Gianella <gianeteo@hotmail.it>
2020-11-11 00:40:20 +0100

    Added sript to test acceptance rate in expansion case

---
+++ tests/test_acceptancerate.R
@@ -0,0 +1,36 @@
+library("SPMIX")
+
+# Generate data (1 location, mixture of 3 normals)
+set.seed(230196)
+means <- c(-5,0,5)
+std_devs <- c(1,1,1)
+weights <- c(0.2,0.5,0.3)
+transformed_weights <- alr(weights)
+data <- list(); data[[1]] <- rep(0,1000)
+for (i in 1:length(means)) {
+  data[[1]] <- data[[1]] + weights[i]*rnorm(1000, means[i], std_devs[i])
+}
+
+# Generate Matrix W
+W <- matrix(0,nrow = 1, ncol = 1, byrow = T)
+
+# Read Params
+params_filename <- system.file("input_files/sampler_params.asciipb", package = "SPMIX")
+params <- readASCII(SamplerParams, file(params_filename))
+
+# Generate state
+readProtoFiles(system.file("proto/univariate_mixture_state.proto", package = "SPMIX"))
+state <- new(UnivariateState)
+state$num_components <- 2
+state$atoms <- list(new(UnivariateMixtureAtom, mean = means[1], stdev = std_devs[1]),
+                    new(UnivariateMixtureAtom, mean = means[2], stdev = std_devs[2]))
+state$groupParams <- new(UnivariateState.GroupParams, weights = inv_alr(alr(weights[1:2])))
+state$rho <- 0.9
+state$Sigma <- new(EigenMatrix, rows=1, cols=1, data=1.2)
+
+# Read options
+options_filename <- system.file("input_files/optimization_options.asciipb", package = "SPMIX")
+options <- readASCII(OptimOptions, file(options_filename))
+
+# Run test
+IncreaseMove_test(data, W, params, state, options)
