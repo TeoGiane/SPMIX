a6b0cec3ebfc5eef7a673f54cce34b1ec6e61278
Matteo Gianella <gianeteo@hotmail.it>
2020-11-13 10:01:51 +0100

    Fixed bug in data simulation

--- tests/test_acceptancerates.R
+++ tests/test_acceptancerates.R
@@ -6,14 +6,10 @@ readProtoFiles(system.file("proto/univariate_mixture_state.proto", package = "SP
 
 # Generate data (1 location, mixture of 3 normals)
 set.seed(230196)
-means <- c(1,2,4)
-std_devs <- c(1,1,1)
-weights <- c(1/3,1/3,1/3)
-transformed_weights <- alr(weights)
-data <- list(); data[[1]] <- rep(0,1000)
-for (i in 1:length(means)) {
-  data[[1]] <- data[[1]] + weights[i]*rnorm(1000, means[i], std_devs[i])
-}
+ngroups <- 1; ncomponents <- 3; N <- 1000
+means <- c(1,4,2); std_devs <- c(1,1,1); weights <- c(1/3,1/3,1/3)
+cluster_alloc <- sample(1:ncomponents, prob = weights, size = N, replace = T)
+data <- list(); data[[1]] <- rnorm(N, mean = means[cluster_alloc], sd = std_devs[cluster_alloc])
 
 # Generate Matrix W
 W <- matrix(0,nrow = 1, ncol = 1, byrow = T)
@@ -30,10 +26,10 @@ options <- readASCII(OptimOptions, file(options_filename))
 # Case 1 - A reduced state, Increase move ---------------------------------
 # Generate the state
 state <- new(UnivariateState)
-state$num_components <- 2
+state$num_components <- ncomponents-1
 state$atoms <- list(new(UnivariateMixtureAtom, mean = means[1], stdev = std_devs[1]),
                     new(UnivariateMixtureAtom, mean = means[2], stdev = std_devs[2]))
-state$groupParams <- new(UnivariateState.GroupParams, weights = inv_alr(alr(weights[1:2])))
+state$groupParams <- new(UnivariateState.GroupParams, weights = inv_alr(alr(weights[1:ncomponents])))
 state$rho <- 0.9
 state$Sigma <- new(EigenMatrix, rows=1, cols=1, data=1.2)
 
@@ -43,10 +39,10 @@ IncreaseMove_test(data, W, params, state, options)
 ###########################################################################
 
 ###########################################################################
-# Case 2 - The right state, Increase move ---------------------------------
+# Case 2 - The right state, Increase and reduce move ----------------------
 # Generate the state
 state <- new(UnivariateState)
-state$num_components <- 3
+state$num_components <- ncomponents
 state$atoms <- list(new(UnivariateMixtureAtom, mean = means[1], stdev = std_devs[1]),
                     new(UnivariateMixtureAtom, mean = means[2], stdev = std_devs[2]),
                     new(UnivariateMixtureAtom, mean = means[3], stdev = std_devs[3]))
@@ -57,3 +53,5 @@ state$Sigma <- new(EigenMatrix, rows=2, cols=2, data=c(1.2,0,0,1.2))
 # Run tests
 IncreaseMove_test(data, W, params, state, options)
 ReduceMove_test(data, W, params, state, options)
+
+###########################################################################
