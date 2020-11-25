595daa72e43852dae0971b4ecbc2e4942bf170d3
Matteo Gianella <gianeteo@hotmail.it>
2020-11-26 00:15:16 +0100

    Changed test API during debugging

--- tests/test_loglik.R
+++ tests/test_loglik.R
@@ -11,4 +11,5 @@ params <- RProtoBuf::readASCII(SamplerParams, file(params_filename))
 load(system.file("input_files/output_chains_serialized.dat", package = "SPMIX"))
 state <- out[[1]]; rm(list='out'); state <- unserialize_proto("UnivariateState", state)
 
-test_spmixloglikelihood(data, W, params, state)
+params_str <- toString(params); state_str <- toString(state);
+test_spmixloglikelihood(data, W, params_str, state_str)
