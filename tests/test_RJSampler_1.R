8491ac26d4d62e5fd5ddd308a07ceeeb9e2e09ed
Matteo Gianella <gianeteo@hotmail.it>
2020-11-28 21:31:50 +0100

    Improved test script

--- tests/test_RJSampler_1.R
+++ tests/test_RJSampler_1.R
@@ -7,7 +7,7 @@ readProtoFiles(system.file("proto/univariate_mixture_state.proto", package = "SP
 # Generate data (1 location, mixture of 3 normals)
 set.seed(230196)
 ngroups <- 1; ncomponents <- 3; N <- 1000
-means <- c(-2,0,5); std_devs <- c(1,1,1); weights <- c(1/6,3/6,2/6)
+means <- c(-2,1,5); std_devs <- c(0.5,1,0.25); weights <- c(1/6,3/6,2/6)
 cluster_alloc <- sample(1:ncomponents, prob = weights, size = N, replace = T)
 data <- list(); data[[1]] <- rnorm(N, mean = means[cluster_alloc], sd = std_devs[cluster_alloc])
 
@@ -16,33 +16,35 @@ W <- matrix(0, nrow = 1, ncol = 1, byrow = T)
 
 # Run Sampler
 # Setting MCMC parameters
-burnin = 5000
-niter = 5000
+burnin = 2000
+niter = 2000
 thin = 2
 
 # Grab input filenames
 params_filename = system.file("input_files/rjsampler_params.asciipb", package = "SPMIX")
 
 # Run Spatial sampler
-out <- SPMIX_sampler(burnin, niter, thin, data, W, params_filename, type = "rjmcmc")
+out <- SPMIX_sampler(burnin, niter, thin, data, W, params_filename, type = "rjmcmc")#, display_progress = F)
+# save(out, file="output.txt")
 # save(out, file = "RJTest1_output_10k.dat")
-save(out, file = "RJTest1_output_10k_noswitch.dat")
+# save(out, file = "RJTest1_output_10k_noswitch.dat")
 
 # Analyses
+load("./tests/output.txt")
 chains <- sapply(out, function(x) unserialize_proto("UnivariateState",x))
 H_chain <- sapply(chains, function(x) x$num_components)
 means_chain <- sapply(chains, function(x) sapply(x$atoms, function(x) x$mean))
 stdev_chain <- sapply(chains, function(x) sapply(x$atoms, function(x) x$stdev))
 weights_chain <- sapply(chains, function(x) x$groupParams[[1]]$weights)
-allocs_chain <- sapply(chains, function(x) table(x$groupParams[[1]]$cluster_allocs))
+# allocs_chain <- sapply(chains, function(x) table(x$groupParams[[1]]$cluster_allocs))
 
 # Barplot for the number of components
-x11(height = 4, width = 8.27); barplot(table(H_chain)/length(chains),ylim = c(0,0.5))
+x11(height = 4, width = 8.27); barplot(table(H_chain)/length(chains))
 title("Posterior of H")
 
 # Plotting the average density over iterations and compare with true curve
 # Computing average density
-x_grid <- seq(-5,5,length.out = 500)
+x_grid <- seq(-5,7,length.out = 500)
 est_dens <- rep(0,length(x_grid))
 for (i in 1:length(chains)) {
   xgrid_expand <- t(rbind(replicate(H_chain[i], x_grid, simplify = "matrix")))
@@ -57,8 +59,7 @@ true_dens <- t(as.matrix(weights)) %*% dnorm(xgrid_expand,means,std_devs)
 
 # Visualization (first sketch)
 x11(height = 4, width = 8.27)
-plot(x_grid, est_dens, type = 'l', col='darkorange', lwd=2, lty=1,
-     xlab = "Grid", ylab = "Density")
-lines(x_grid, true_dens, col='blue', lwd=2, lty=1)
-legend("bottomright", legend = c("Estimated", "True"), col=c("darkorange","blue"), lty = 1, lwd = 2, )
+plot(x_grid, true_dens, type = 'l', col='blue', lwd=2, lty=1,xlab = "Grid", ylab = "Density")
+lines(x_grid, est_dens, col='darkorange', lwd=2, lty=1)
+legend("topleft", legend = c("True", "Estimated"), col=c("blue","darkorange"), lty = 1, lwd = 2)
 title("Area 1 - Density estimation")
