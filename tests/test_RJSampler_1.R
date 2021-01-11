7787b4d20b6730e26de70c94df615a6cf653f7aa
Matteo Gianella <gianeteo@hotmail.it>
2021-01-11 18:16:13 +0100

    Added ggplot2 visualization

--- tests/test_RJSampler_1.R
+++ tests/test_RJSampler_1.R
@@ -2,7 +2,22 @@
 
 # Required libraries
 library("SPMIX")
+library("ggplot2")
 
+# Utilites
+DataRecast <- function(x, y, labels = row.names(y)) {
+  rows <- dim(y)[1]; cols <- dim(y)[2]
+  out <- data.frame()
+  for (i in 1:rows) {
+    tmp <- data.frame(x, y[i,], as.factor(rep(labels[i],cols)));
+    names(tmp) <- c("Grid", "Value", "Density"); row.names(tmp) <- NULL
+    out <- rbind(out,tmp)
+  }
+  return(out)
+}
+
+###########################################################################
+# Data Simulation ---------------------------------------------------------
 # Generate data (1 location, mixture of 3 normals)
 set.seed(230196)
 ngroups <- 1; ncomponents <- 3; N <- 1000
@@ -13,28 +28,55 @@ data <- list(); data[[1]] <- rnorm(N, mean = means[cluster_alloc], sd = std_devs
 # Generate Matrix W
 W <- matrix(0, nrow = 1, ncol = 1, byrow = T)
 
-# Run Sampler
+###########################################################################
+
+###########################################################################
+# Sampler Run -------------------------------------------------------------
 # Setting MCMC parameters
-burnin = 5000
-niter = 5000
-thin = 2
+burninfull = 0;     burnin = 5000
+niterfull = 10000;  niter = 5000
+thinfull = 1;       thin = 2
 
 # Grab input filenames
 params_filename = system.file("input_files/rjsampler_params.asciipb", package = "SPMIX")
 
 # Run Spatial sampler
-out <- SPMIXSampler(burnin, niter, thin, data, W, params_filename, type = "rjmcmc")
+out <- SPMIXSampler(burninfull, niterfull, thinfull, data, W, params_filename, type = "rjmcmc")
 
-# Analyses
+###########################################################################
+
+###########################################################################
+# Analyses and Visualization ----------------------------------------------
+
+# Deserialization
 chains <- sapply(out, function(x) DeserializeSPMIXProto("UnivariateState",x))
 H_chain <- sapply(chains, function(x) x$num_components)
-means_chain <- lapply(chains, function(x) sapply(x$atoms, function(x) x$mean))
-stdev_chain <- lapply(chains, function(x) sapply(x$atoms, function(x) x$stdev))
-weights_chain <- lapply(chains, function(x) x$groupParams[[1]]$weights)
 
-# Barplot for the number of components
-x11(height = 4, width = 8.27); barplot(table(H_chain)/length(chains))
-title("Posterior of H")
+# Traceplot for the whole chain (no burnin, no thinning)
+df <- data.frame("Iteration"=1:niterfull, "LowPoints"=H_chain-0.3, "UpPoints"=H_chain+0.3)
+plot_traceH <- ggplot(data=df, aes(x=Iteration, y=LowPoints, xend=Iteration, yend=UpPoints)) +
+  ylim(range(df[,-1])) + ylab("NumComponents") + geom_segment(lwd=0.1) +
+  theme(plot.title = element_text(face="bold", hjust = 0.5), plot.subtitle = element_text(hjust = 0.5)) +
+  ggtitle("Traceplot of H")
+rm(list='df')
+
+x11(height = 3, width = 3); plot_traceH
+
+# Reducing chain according to burnin, niter and thin params
+fullout <- out
+out <- fullout[burnin+1:length(fullout)]; out<-out[!sapply(out,is.null)]; out <- out[which((1:length(out)) %% thin == 1)]
+chains <- sapply(out, function(x) DeserializeSPMIXProto("UnivariateState",x))
+H_chain <- sapply(chains, function(x) x$num_components)
+
+# Barplot of the estimated posterior for H
+df <- as.data.frame(table(H_chain)/length(H_chain)); names(df) <- c("NumComponents", "Prob.")
+plot_postH <- ggplot(data = df, aes(x=NumComponents, y=Prob.)) +
+  geom_bar(stat="identity", color="steelblue", fill="lightblue") +#"white") +
+  theme(plot.title = element_text(face="bold", hjust = 0.5), plot.subtitle = element_text(hjust = 0.5)) +
+  ggtitle("Posterior of H")
+rm(list='df')
+
+x11(height = 3, width = 3); plot_postH
 
 # Plotting the average density over iterations and compare with true curve
 # Computing estimated density
@@ -46,9 +88,12 @@ x_grid <- seq(data_range[1,1], data_range[2,1], length.out = 500)
 xgrid_expand <- t(rbind(replicate(ncomponents, x_grid, simplify = "matrix")))
 true_dens <- t(as.matrix(weights)) %*% dnorm(xgrid_expand,means,std_devs)
 
-# Visualization (first sketch)
-x11(height = 4, width = 8.27)
-plot(x_grid, true_dens, type = 'l', col='blue', lwd=2, lty=1,xlab = "Grid", ylab = "Density")
-lines(x_grid, est_dens[[1]], col='darkorange', lwd=2, lty=1)
-legend("topleft", legend = c("True", "Estimated"), col=c("blue","darkorange"), lty = 1, lwd = 2)
-title("Area 1 - Density estimation")
+# Density comparison - visual plot
+y <- rbind(est_dens[[1]], true_dens); row.names(y) <- c("Estimated", "True")
+plot_densCompare <- ggplot(data = DataRecast(x_grid,y), aes(x=Grid, y=Value, group=Density, col=Density)) +
+  geom_line(lwd=1) + theme(plot.title = element_text(face="bold", hjust = 0.5)) +
+  ggtitle("Area 1")
+
+x11(width = 5.8, height = 3); plot_densCompare
+
+###########################################################################
