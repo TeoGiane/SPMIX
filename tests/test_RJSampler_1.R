f47c77fc37ee0975628957b1b7c858d3c5d57735
Matteo Gianella <gianeteo@hotmail.it>
2021-01-12 12:12:01 +0100

    Added sampler runs with various number of observations

--- tests/test_RJSampler_1.R
+++ tests/test_RJSampler_1.R
@@ -97,3 +97,47 @@ plot_densCompare <- ggplot(data = DataRecast(x_grid,y), aes(x=Grid, y=Value, gro
 x11(width = 5.8, height = 3); plot_densCompare
 
 ###########################################################################
+
+###########################################################################
+# Sensitivity w.r.t. number of observations -------------------------------
+
+# Clean console and set number of observations
+rm(list=ls()); set.seed(230196)
+Ns <- c(100,250,500,1000)
+W <- matrix(0, nrow = 1, ncol = 1, byrow = T)
+plots <- list()
+
+# Sampler run loop
+for (i in 1:length(Ns)) {
+  # Generate data (1 location, mixture of 3 normals)
+  ngroups <- 1; ncomponents <- 3; N <- Ns[i]
+  means <- c(-4,1,5); std_devs <- c(1,1,1); weights <- c(1/6,3/6,2/6)
+  cluster_alloc <- sample(1:ncomponents, prob = weights, size = N, replace = T)
+  data <- list(); data[[1]] <- rnorm(N, mean = means[cluster_alloc], sd = std_devs[cluster_alloc])
+
+  # Grab input filenames
+  params_filename = system.file("input_files/rjsampler_params.asciipb", package = "SPMIX")
+
+  # Run Spatial sampler
+  out <- SPMIXSampler(0, 10000, 1, data, W, params_filename, type = "rjmcmc")
+
+  # Deserialization
+  chains <- sapply(out, function(x) DeserializeSPMIXProto("UnivariateState",x))
+  H_chain <- sapply(chains, function(x) x$num_components)
+
+  # Traceplot for the whole chain (no burnin, no thinning)
+  df <- data.frame("Iteration"=1:10000, "LowPoints"=H_chain-0.3, "UpPoints"=H_chain+0.3)
+  tmp <- ggplot(data=df, aes(x=Iteration, y=LowPoints, xend=Iteration, yend=UpPoints)) +
+    ylim(range(df[,-1])) + ylab("NumComponents") + geom_segment(lwd=0.1) +
+    theme(plot.title = element_text(face="bold", hjust = 0.5), plot.subtitle = element_text(hjust = 0.5)) +
+    ggtitle(paste0("Traceplot of H - ", Ns[i], " obs."))
+  rm(list='df')
+
+  # Save plot
+  plots[[i]] <- tmp; rm(list='tmp')
+}
+
+# Visualization
+x11(height = 6, width = 6); gridExtra::grid.arrange(grobs=plots, nrow=2, ncol=2)
+
+###########################################################################
