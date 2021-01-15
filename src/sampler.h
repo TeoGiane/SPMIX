504c12bcdda902c9dc58808a9c134f756fe31365
Matteo Gianella <gianeteo@hotmail.it>
2021-01-15 10:25:17 +0100

    Cleaned code for better readability

--- src/sampler.h
+++ src/sampler.h
@@ -1,138 +1,31 @@
 #ifndef SRC_SAMPLER_HPP
 #define SRC_SAMPLER_HPP
 
-/*#include <algorithm>
-#include <random>
-#include <vector>
-#include <numeric>
-#include <stdexcept>
-#include <exception>
-#include <Eigen/Dense>
-#include <unsupported/Eigen/KroneckerProduct>
-#include <stan/math/prim/mat.hpp>
-
-#include "collector.h"
-#include "univariate_mixture_state.pb.h"
-#include "sampler_params.pb.h"
-#include "PolyaGammaHybrid.h"
-#include "mcmc_utils.h"
-#include "utils.h"*/
 #include "sampler_base.h"
 
 class SpatialMixtureSampler: public SpatialMixtureSamplerBase {
- protected:
-     // params
-     //SamplerParams params;
+  protected:
 
-     // data
-     /*int numGroups;
-     std::vector<int> samplesPerGroup;
-     std::vector<std::vector<double>> data;
-     int numdata;
-     Eigen::MatrixXd W_init;*/
+	// prior for Sigma --> here is an InvWishart
+	double nu;
+	Eigen::MatrixXd V0;
 
-     // mixtures
-     /*int numComponents;
-     std::vector<double> means;
-     std::vector<double> stddevs;*/
+  public:
+	SpatialMixtureSampler() {}
 
-     /*Eigen::MatrixXd weights; // one set of weights per location
-     Eigen::MatrixXd transformed_weights;
-     std::vector<std::vector<int>> cluster_allocs;
-     Eigen::MatrixXd mtildes;
-     int num_connected_comps;
+	SpatialMixtureSampler(
+		const SamplerParams &_params,
+		const std::vector<std::vector<double>> &_data,
+		const Eigen::MatrixXd &W);
 
-     // MCAR
-     double rho;
-     Eigen::MatrixXd Sigma;
-     Eigen::MatrixXd W;
-     Eigen::MatrixXd SigmaInv;
-     Eigen::MatrixXd F;
-     std::vector<int> node2comp;
-     double mtilde_sigmasq = 1.0;
-     std::vector<std::vector<int>> comp2node;
-     std::vector<Eigen::MatrixXd> F_by_comp;
-     std::vector<Eigen::MatrixXd> G_by_comp;
-
-     // Regression
-     bool regression = false;
-     int p_size;
-     Eigen::VectorXd reg_coeff;
-     Eigen::MatrixXd predictors;
-     Eigen::VectorXd reg_coeff_mean;
-     Eigen::MatrixXd reg_coeff_prec;
-     Eigen::VectorXd reg_data;
-     Eigen::VectorXd mu;
-     Eigen::DiagonalMatrix<double, Eigen::Dynamic, Eigen::Dynamic> V;*/
-
-     // prior for Sigma --> here is an InvWishart
-     double nu;
-     Eigen::MatrixXd V0;
-
-     // prior for Rho
-     /*double alpha;
-     double beta;
-
-     // adaptive MCMC for rho
-     double sigma_n_rho;
-     double rho_sum;
-     double rho_sum_sq;
-     int iter = 0;
-
-     std::vector<Eigen::VectorXd> pippo;
-     Eigen::MatrixXd sigma_star_h;
-     
-     // HyperParams for NormalGamma
-     double priorMean, priorA, priorB, priorLambda;
-
-     unsigned long seed = 213513435;
-     PolyaGammaHybridDouble* pg_rng = nullptr;
-     std::mt19937_64 rng{213513435};
-
-     // diagnostic for the MH sampler
-     int numAccepted = 0;*/
-
-
- public:
-     SpatialMixtureSampler() {}
-
-     SpatialMixtureSampler(
-        const SamplerParams &_params,
-        const std::vector<std::vector<double>> &_data,
-        const Eigen::MatrixXd &W);
-
-    SpatialMixtureSampler(
-       const SamplerParams &_params,
-       const std::vector<std::vector<double>> &_data,
-       const Eigen::MatrixXd &W, const std::vector<Eigen::MatrixXd> &X);
-
-    /*~SpatialMixtureSampler() {
-        delete(pg_rng);
-    }*/
+	SpatialMixtureSampler(
+		const SamplerParams &_params,
+		const std::vector<std::vector<double>> &_data,
+		const Eigen::MatrixXd &W, const std::vector<Eigen::MatrixXd> &X);
 
     void init();
 
     void sample() override;
-    /*
-     * We use a Normal kernel with conjugate Normal - Inverse Gamma
-     * base measure, so the update of the atom is
-     * Law(\mu, \sigma)_h \propto
-     *  P_0(\mu, \sigma) \prod_{(i, j): s_{ij}=h} N(y_{ij} | \mu_h, \sigma_h)
-     * That is a conjugate normal likelihood with normal inverse gamma prior
-     */
-    //void sampleAtoms();
-
-    //void sampleAllocations();
-
-    /*
-     * We use the PolyaGamma trick to sample from the transformed weights
-     */
-    //void sampleWeights();
-
-    /*
-     * This step requires a Metropolis Hastings step
-     */
-    //void sampleRho();
 
     /*
      * We use a conjugate Inverse - Wishart prior for Sigma, so the
@@ -143,26 +36,7 @@ class SpatialMixtureSampler: public SpatialMixtureSamplerBase {
      */
     void sampleSigma() override;
 
-    //void regress();
-
-    //void computeRegressionResiduals();
-
-    //void _computeInvSigmaH();
-
-    /*
-     * Sampler the hyperparameters in the base measure P_0
-     */
-    //void sampleHyperParams();
-
     void sample_mtilde();
-
-    //void saveState(Collector<UnivariateState>* collector);
-
-    //UnivariateState getStateAsProto();
-
-    //void printDebugString();
-
-    //const int& getNumAccepted() {return numAccepted;}
 };
 
 #endif // SRC_SAMPLER_HPP
